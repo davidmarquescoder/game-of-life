1b90cf8aeda76cb2ee025bcf018d3ecc2ee2cea5
David Marques <davidmarques99@hotmail.com>
2024-04-25 18:29:29 -0300

    Two functions were created, one to initially map the values on the grid, defining the live cells (1) and the dead cells (0) and another function to draw the grid and the cells on the screen.

--- src/script.js
+++ src/script.js
@@ -28,3 +28,31 @@ function countCells(matriz) {
 
     return { dead, alive };
 }
+
+// Criando o grid
+function createGrid() {
+  for (let i = 0; i < rows; i++) {
+    grid[i] = [];
+
+    for (let j = 0; j < cols; j++) {
+      grid[i][j] = Math.random() > 0.5 ? 1 : 0;
+    }
+  }
+}
+
+// Desenhando as células no grid
+function drawGrid() {
+  const gridContainer = document.getElementById('grid-container');
+  gridContainer.innerHTML = '';
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      const cell = document.createElement('div');
+      cell.className = grid[i][j] ? 'alive' : 'dead';
+      gridContainer.appendChild(cell);
+    }
+  }
+  
+  aliveCount.innerHTML = countCells(grid).alive;
+  deadCount.innerHTML = countCells(grid).dead;
+}

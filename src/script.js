3a83a9bf005ead592c50b1ded9fd972474d853ae
David Marques <davidmarques99@hotmail.com>
2024-04-25 18:45:56 -0300

    created function to count number of neighbors of a cell and function to create next generation.

--- src/script.js
+++ src/script.js
@@ -11,22 +11,20 @@ const deadCount = document.getElementById('dead-count').querySelector('span');
 
 // Contagem de vivos e mortos
 function countCells(matriz) {
-    let alive = 0;
-    let dead = 0;
+  let alive = 0;
+  let dead = 0;
 
-    for (let i = 0; i < matriz.length; i++) {
-        for (let j = 0; j < matriz[i].length; j++) {
-            if (matriz[i][j] === 0) {
-                dead++;
-            }
-
-            else if (matriz[i][j] === 1) {
-                alive++;
-            }
-        }
+  for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+      if (matriz[i][j] === 0) {
+        dead++;
+      } else if (matriz[i][j] === 1) {
+        alive++;
+      }
     }
+  }
 
-    return { dead, alive };
+  return { dead, alive };
 }
 
 // Criando o grid
@@ -52,7 +50,50 @@ function drawGrid() {
       gridContainer.appendChild(cell);
     }
   }
-  
+
   aliveCount.innerHTML = countCells(grid).alive;
   deadCount.innerHTML = countCells(grid).dead;
 }
+
+// Contagem de vizinhos
+function countNeighbors(x, y) {
+  let count = 0;
+  for (let i = -1; i <= 1; i++) {
+    for (let j = -1; j <= 1; j++) {
+      const row = (x + i + rows) % rows;
+      const col = (y + j + cols) % cols;
+      count += grid[row][col];
+    }
+  }
+  
+  count -= grid[x][y];
+  return count;
+}
+
+// Próxima geração de células
+function nextGeneration() {
+  // Cria uma cópia do grid
+  let newGrid = JSON.parse(JSON.stringify(grid));
+
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      let neighbors = countNeighbors(i, j);
+
+      if (grid[i][j] === 1) {
+        if (neighbors < 2 || neighbors > 3) {
+          // Morte por solidão ou superpopulação
+          newGrid[i][j] = 0;
+        }
+      } else if (neighbors === 3){
+        // Nascimento
+        newGrid[i][j] = 1;
+      }
+    }
+  }
+
+  grid = newGrid;
+}
+
+// Inicialização
+createGrid()
+drawGrid()

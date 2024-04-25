45236e4093916fbbb34319e31e1fc4a7c8ebfd11
David Marques <davidmarques99@hotmail.com>
2024-04-25 18:23:06 -0300

    add a function to count cells and created .editorconfig config file

--- src/script.js
+++ src/script.js
@@ -8,3 +8,23 @@ let grid = [];
 // Definindo os elementos que apresentarão algumas métricas na tela.
 const aliveCount = document.getElementById('alive-count').querySelector('span');
 const deadCount = document.getElementById('dead-count').querySelector('span');
+
+// Contagem de vivos e mortos
+function countCells(matriz) {
+    let alive = 0;
+    let dead = 0;
+
+    for (let i = 0; i < matriz.length; i++) {
+        for (let j = 0; j < matriz[i].length; j++) {
+            if (matriz[i][j] === 0) {
+                dead++;
+            }
+
+            else if (matriz[i][j] === 1) {
+                alive++;
+            }
+        }
+    }
+
+    return { dead, alive };
+}
